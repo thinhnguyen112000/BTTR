88cfdb196899d84142581284c1bf181641ba606d
Rysh <nghiathinh2000@gmail,com>
2022-01-08 14:41:58 +0700

    Update vocal and dataloader

--- bttr/datamodule/vocab.py
+++ bttr/datamodule/vocab.py
@@ -1,43 +1,38 @@
+import json
 import os
 from functools import lru_cache
 from typing import Dict, List
-
+from transformers import PreTrainedTokenizerFast
 
 @lru_cache()
 def default_dict():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "dictionary.txt")
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "tokenizer.json")
 
 
 class CROHMEVocab:
-
-    PAD_IDX = 0
-    SOS_IDX = 1
-    EOS_IDX = 2
-
     def __init__(self, dict_path: str = default_dict()) -> None:
-        self.word2idx = dict()
-        self.word2idx["<pad>"] = self.PAD_IDX
-        self.word2idx["<sos>"] = self.SOS_IDX
-        self.word2idx["<eos>"] = self.EOS_IDX
-
-        with open(dict_path, "r") as f:
-            for line in f.readlines():
-                w = line.strip()
-                self.word2idx[w] = len(self.word2idx)
-
-        self.idx2word: Dict[int, str] = {v: k for k, v in self.word2idx.items()}
-
-        # print(f"Init vocab with size: {len(self.word2idx)}")
+        self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=dict_path)
+        with open(dict_path, "rb") as f:
+            self.vocab = json.load(f)['model']['vocab']
+        self.PAD_IDX = self.vocab['[PAD]']
+        self.SOS_IDX = self.vocab['[BOS]']
+        self.EOS_IDX = self.vocab['[EOS]']
 
     def words2indices(self, words: List[str]) -> List[int]:
-        return [self.word2idx[w] for w in words]
+        return self.tokenizer(words[0])['input_ids']
 
     def indices2words(self, id_list: List[int]) -> List[str]:
-        return [self.idx2word[i] for i in id_list]
+        return self.tokenizer.convert_ids_to_tokens(id_list)
 
     def indices2label(self, id_list: List[int]) -> str:
         words = self.indices2words(id_list)
         return " ".join(words)
 
     def __len__(self):
-        return len(self.word2idx)
+        return len(self.vocab)
+
+
+if __name__ == '__main__':
+    tex = r"\pi_{1}(T,t_{0})\approx\pi_{1}(S^{1},x_{0})\times\pi_{1}(S^{1},y_{0})\cong\mathbf{Z}\times\mathbf{Z}=\mathbf{Z}^{2}."
+    x = CROHMEVocab()
+    print(x.words2indices([tex]))
