8dd28d898e3ef8b5f16dcb8765181b59b8193b85
Rysh <49011740+thinhnguyen112000@users.noreply.github.com>
2022-01-14 10:12:38 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-wget 'https://drive.google.com/uc?export=download&id=13vr06gv0MzrFmY3NoKLhi7tlHlv_q_pJ' -O train.txt
-wget 'https://drive.google.com/uc?export=download&id=126G2yjVJdbgZlK18V_4QHdMv8Q7xZlpU' -O val.txt
-wget 'https://drive.google.com/uc?export=download&id=1UwPx3xgdQqRqyURkqptWMwJsZqIt41d1' -O test.txt
-wget 'https://drive.google.com/uc?export=download&id=1mUrzZnwRurvcd7XFQ9ky6nYt1P9gssef' -O ProcessImage.zip
-unzip ProcessImage.zip
-rm ProcessImage.zip
-
-pip install -r requirements.txt
-python train.py --config config.yaml
